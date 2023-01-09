0ffc215eaf3ce88a3605abd9e8805df3df64a900
FR_Jess <fr_jess@icloud.com>
2023-01-09 15:27:28 +0100

    [add] active navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,10 @@
 <div class="navbar">
   <ul class="d-flex">
-    <li class="px-3 active"><a href="{{ route('home') }}">Home</a></li>
-    <li class="px-3"><a href="{{ route('about') }}">About us</a></li>
-    <li class="px-3"><a href="{{ route('contacts') }}">Contatti</a></li>
+    <li class="px-3 {{ Route::currentRouteName() === 'home' ? 'active' : '' }}"><a href="{{ route('home') }}">Home</a>
+    </li>
+    <li class="px-3 {{ Route::currentRouteName() === 'about' ? 'active' : '' }}"><a href="{{ route('about') }}">About
+        us</a></li>
+    <li class="px-3 {{ Route::currentRouteName() === 'contacts' ? 'active' : '' }}"><a
+        href="{{ route('contacts') }}">Contatti</a></li>
   </ul>
 </div>
